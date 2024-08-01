d5d2ed1e2737036bc02ffd7f60b4a55fd1f9f322
incham <dlscks0558@naver.com>
2024-08-02 03:33:14 +0900

    next config 수정

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,10 @@ const nextConfig = {
   typescript: {
     tsconfigPath: path.resolve('./tsconfig.json'),
   },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
 };
 
 export default nextConfig;
