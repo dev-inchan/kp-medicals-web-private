9c51bb8ae79c953421127ab689bdacc0c03e9e49
incham <dlscks0558@naver.com>
2024-10-02 20:47:22 +0900

    msw

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -1,7 +1,18 @@
-import { setupWorker } from 'msw/browser';
-import { handlers } from './handlers';
+// import { setupWorker } from 'msw/browser';
+// import { handlers } from './handlers';
 
-// This configures a Service Worker with the given request handlers.
-const worker = setupWorker(...handlers);
+// // This configures a Service Worker with the given request handlers.
+// const worker = setupWorker(...handlers);
+
+// export default worker;
+let worker;
+
+if (process.env.NODE_ENV === 'development') {
+  // 개발 환경에서만 MSW를 설정합니다.
+  const { setupWorker } = require('msw/browser');
+  const { handlers } = require('./handlers');
+
+  worker = setupWorker(...handlers);
+}
 
 export default worker;
